ed3bac410596c8c3c245700f9bc0dd6c6bb26c18
pokemonDC <100698945+pokemonDC@users.noreply.github.com>
2022-03-01 23:19:37 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,32 +1,34 @@
+<script type="text/javascript">
+
 var submit = document.getElementById('submitButton');
-submit.onclick = showImage;     //Submit 버튼 클릭시 이미지 보여주기
+submit.onclick = showImage(this);     //Submit 버튼 클릭시
 
-function showImage() {
-    var newImage = document.getElementById('image-show').lastElementChild;
-    newImage.style.visibility = "visible";
-    
-    document.getElementById('image-upload').style.visibility = 'hidden';
-
-    document.getElementById('fileName').textContent = null;     //기존 파일 이름 지우기
+function showImage(f) {
+    // 업로드 할 수 있는 파일 확장자를 제한합니다.
+	var extArray = new Array('hwp','xls','doc','xlsx','docx','pdf','jpg','gif','png','txt','ppt','pptx');
+	var path = document.getElementById("upfile").value;
+	if(path == "") {
+		alert("파일을 선택해 주세요.");
+		return false;
+	}
+	var pos = path.indexOf(".");
+	if(pos < 0) {
+		alert("확장자가 없는파일 입니다.");
+		return false;
+	}
+	var ext = path.slice(path.indexOf(".") + 1).toLowerCase();
+	var checkExt = false;
+	for(var i = 0; i < extArray.length; i++) {
+		if(ext == extArray[i]) {
+			checkExt = true;
+			break;
+		}
+	}
+	if(checkExt == false) {
+		alert("업로드 할 수 없는 파일 확장자 입니다.");
+	    return false;
+	}
+	return true;
 }
 
-
-function loadFile(input) {
-    var file = input.files[0];
-
-    var name = document.getElementById('fileName');
-    name.textContent = file.name;
-
-    var newImage = document.createElement("img");
-    newImage.setAttribute("class", 'img');
-
-    newImage.src = URL.createObjectURL(file);   
-
-    newImage.style.width = "70%";
-    newImage.style.height = "70%";
-    newImage.style.visibility = "hidden";   //버튼을 누르기 전까지는 이미지 숨기기
-    newImage.style.objectFit = "contain";
-
-    var container = document.getElementById('image-show');
-    container.appendChild(newImage);
-};
+</script>
